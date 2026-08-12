be6983dd241c6df8e5f2aeac514ff9c4b3702d2c
j-camposar <31163773+j-camposar@users.noreply.github.com>
2026-08-12 10:54:26 -0400

    V1.1.0 : creacion de endpoint despedida

--- src/main/java/com/example/versionamientov2/Controller/HolaMundoV1.java
+++ src/main/java/com/example/versionamientov2/Controller/HolaMundoV1.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -15,4 +18,9 @@ public class HolaMundoV1 {
     public String saludo() {
         return "hola mundo";
     }
+    @PostMapping
+    public String despedida() {
+        return "adios v1.1.0";
+    }
+    
 }
